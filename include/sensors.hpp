a003ffa02be2eb04136d5d3f72b6fb752930218a
Tad Keller <logisch@protonmail.com>
2020-06-04 16:29:57 -0500

    added some vision sensor code and added sensor files

---
+++ include/sensors.hpp
@@ -0,0 +1,3 @@
+#include"main.h"
+
+ void vision_READ(pros::vision_signature_s_t sig, int MAX_LEFT, int MAX_RIGHT, bool aton);
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,22 @@
 #include "main.h"
 #include"display/lvgl.h"
 
-void initialize() {
 
+
+void initialize()
+{
 }
 
-void disabled() {}
+void disabled()
+ {}
 
 
-void competition_initialize() {}
+void competition_initialize()
+ {}
 
 
-void autonomous() {}
-
+void autonomous()
+{}
 
 
 //toggles for flywheel
@@ -35,6 +39,8 @@ void opcontrol()
 	lv_obj_set_pos(im, 0, 0);
 	lv_obj_set_drag(im, true);
 	*/
+
+
 	pros::Controller controller(pros::E_CONTROLLER_MASTER);
 
 	pros::Motor leftDrive(10, pros::E_MOTOR_GEARSET_18, false);
@@ -50,115 +56,113 @@ void opcontrol()
 		leftDrive.move(controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y));
 		rightDrive.move(controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y));
 
-		//FLYWHEEL CONTROLER
-	if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_L1))
-	{
-		if(!flyPressed)
+			//FLYWHEEL CONTROLER
+		if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_L1))
 		{
-			flyToggle = 1 - flyToggle;
+			if(!flyPressed)
+			{
+				flyToggle = 1 - flyToggle;
 
-			flyPressed = 1;
+				flyPressed = 1;
+			}
 		}
-	}
-	else
-		flyPressed = 0;
+		else
+			flyPressed = 0;
 
-	if(flyToggle)
-	{
-		Flywheel.move(127);
-	}
-	else
-  	{
-		Flywheel.move(0);
-  	}
-
-	//LOADING SYSTEM.
-	
-	if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_Y))
-	{
-		rightLoader.move(127);
-		leftLoader.move(127);
-	}
-	else
-	if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_A))
-	{
-		rightLoader.move(-127);
-		leftLoader.move(-127);
-	}
-	else 
-	if(!controller.get_digital(pros::E_CONTROLLER_DIGITAL_Y) && !controller.get_digital(pros::E_CONTROLLER_DIGITAL_A))
-	{
-		rightLoader.move(0);
-		leftLoader.move(0);
-	}
-
-
-
-
-	//go forward with drum
-	if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_L2))
-	{
-		//if the forward button toggle isn't on then continute
-		if(!buttonPressedF)
+		if(flyToggle)
 		{
-			//actaully flip the toggle, this is why the type has to be int
-			buttonToggleF = 1 - buttonToggleF;
-			//changed button pressed to true
-			buttonPressedF = 1;
-			//change the backward toggle to false so we don't try to go backwards and forwards
-			buttonToggleR = false;
+			Flywheel.move(127);
 		}
-	}
-	//switch back to normal buttton state but leave toggle on if button isn't pressed.
-	else
-		buttonPressedF = 0;
-
-//if our forward toggle is on, then eat the balls :D
-if(buttonToggleF == true)
-{
-	Lift.move(90);
-}
-//check if other toggle is on if we need to really stop the motor
-else
-{
-	if(!buttonToggleR && !buttonToggleF)
-	{
-		Lift.move(90);
-	}
-}
-//go backwards with drum
-if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_R2))
-{
-	//if we haven't pressed the button then toggle the button
-	if(!buttonPressedR)
-	{
-		//actually toggle the button, this is why the type is int
-		buttonToggleR = 1 - buttonToggleR;
-
-		buttonPressedR = 1;
-
-		//so we stop going forward.
-		buttonToggleF = false;
-	}
-}
-//else, then turn button pressed to false
-else
-	buttonPressedR = 0;
-
-//if backward button toggle is on, then start the motor backward
-if(buttonToggleR == true)
-{
-	Lift.move(-90);
-}
-	//else, check if the forward toggle is off, then stop.
-	else
-	{
-		if(!buttonToggleF && !buttonToggleR)
+		else
 		{
-			Lift.move(0);
+			Flywheel.move(0);
 		}
-	}
-	pros::Task::delay(10);
-}
 
+		//LOADING SYSTEM.
+
+		if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_Y))
+		{
+			rightLoader.move(127);
+			leftLoader.move(127);
+		}
+		else
+		if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_A))
+		{
+			rightLoader.move(-127);
+			leftLoader.move(-127);
+		}
+		else
+		if(!controller.get_digital(pros::E_CONTROLLER_DIGITAL_Y) && !controller.get_digital(pros::E_CONTROLLER_DIGITAL_A))
+		{
+			rightLoader.move(0);
+			leftLoader.move(0);
+		}
+
+
+		//go forward with drum
+		if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_L2))
+		{
+			//if the forward button toggle isn't on then continute
+			if(!buttonPressedF)
+			{
+				//actaully flip the toggle, this is why the type has to be int
+				buttonToggleF = 1 - buttonToggleF;
+				//changed button pressed to true
+				buttonPressedF = 1;
+				//change the backward toggle to false so we don't try to go backwards and forwards
+				buttonToggleR = false;
+			}
+		}
+		//switch back to normal buttton state but leave toggle on if button isn't pressed.
+		else
+			buttonPressedF = 0;
+
+		//if our forward toggle is on, then eat the balls :D
+		if(buttonToggleF == true)
+		{
+			Lift.move(90);
+		}
+		//check if other toggle is on if we need to really stop the motor
+		else
+		{
+			if(!buttonToggleR && !buttonToggleF)
+			{
+				Lift.move(90);
+			}
+		}
+		//go backwards with drum
+		if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_R2))
+		{
+			//if we haven't pressed the button then toggle the button
+			if(!buttonPressedR)
+			{
+				//actually toggle the button, this is why the type is int
+				buttonToggleR = 1 - buttonToggleR;
+
+				buttonPressedR = 1;
+
+				//so we stop going forward.
+				buttonToggleF = false;
+			}
+		}
+	//else, then turn button pressed to false
+		else
+			buttonPressedR = 0;
+
+		//if backward button toggle is on, then start the motor backward
+		if(buttonToggleR == true)
+		{
+			Lift.move(-90);
+		}
+		//else, check if the forward toggle is off, then stop.
+		else
+		{
+			if(!buttonToggleF && !buttonToggleR)
+			{
+				Lift.move(0);
+			}
+		}
+		//delay program to allow draw calls to display and sensor polling.
+		pros::Task::delay(10);
+	}
 }
