e2bd8702b374771ca8aa738194f5a9fc575e86f5
Tad Keller <logisch@protonmail.com>
2020-06-19 17:50:17 -0500

    changed sensor code to work with both sides based on defines

--- src/sensors.cpp
+++ src/sensors.cpp
@@ -1,12 +1,13 @@
 #include"sensors.hpp"
 
-#define SIG 1
+#define ALLIANCE_SIG 1
+#define ENEMEY_SIG 2
 
 pros::vision_signature_s_t BLUE_SIG = pros::Vision::signature_from_utility(1, -3181, -2279, -2730, 18811, 22221, 20516, 3.000, 0);
 pros::vision_signature_s_t RED_SIG = pros::Vision::signature_from_utility(2, 7711, 9645, 8678, -1089, 79, -505, 2.000, 0);
 pros::Vision vSensor(8, pros::E_VISION_ZERO_CENTER);
 
-//define to change sorting color
+//define the alliance color to sort the correct ball color.
 #define BLUE
 
 bool SORT_SYS_ENABLE = true;
@@ -15,24 +16,27 @@ bool SORT_SYS_ENABLE = true;
 void sort(void* sigPass)
 {
     pros::vision_signature_s_t sig =  *reinterpret_cast<pros::vision_signature_s_t*>(sigPass);
-	//basically resetting the vision sensor.
+	//resetting vision sensor LED color.
 	vSensor.clear_led();
 
-	//set SIG as sig so SIG can be referenced later
-	vSensor.set_signature(SIG, &sig);
+	//set the red and blue signatures to be referenced later.
+	vSensor.set_signature(ALLIANCE_SIG, &sig);
 	#ifdef BLUE
-	vSensor.set_signature(2, &RED_SIG);
+	vSensor.set_signature(ENEMEY_SIG, &RED_SIG);
+	#else
+	vSensor.set_signature(ENEMEY_SIG, &BLUE_SIG);
 	#endif
+
 	while(true)
 	{
         //if the sorting system is disabled then don't attemp to sort.
         if(!SORT_SYS_ENABLE)
             break;
 		//get the largest object(0), based on the signature passed in.
-		pros::vision_object_s_t First_rtn = vSensor.get_by_sig(0, SIG);
-		pros::vision_object_s_t Second_rtn = vSensor.get_by_sig(0, 2);
+		pros::vision_object_s_t First_rtn = vSensor.get_by_sig(0, ALLIANCE_SIG);
+		pros::vision_object_s_t Second_rtn = vSensor.get_by_sig(0, ENEMEY_SIG);
 
-        /*the two if statements simply reverse the bottom lift to eject the ball if it is of the enemy color*/
+        //these if statements simply sort out the enemy ball colors
 		#ifdef BLUE
         //255 returns if no objects of stated signature is found.
 		if(Second_rtn.signature != 255 && First_rtn.signature != 255)
@@ -51,12 +55,15 @@ void sort(void* sigPass)
             	bottomLift.move(127);
 				Lift.move(127);
 			}
-			
 		}
 		else
 		if(First_rtn.signature != 255)
 		{
+			#ifdef BLUE
 			vSensor.set_led(COLOR_BLUE);
+			#else
+			vSensor.set_led(COLOR_RED);
+			#endif
 			topLift.move(127);
             bottomLift.move(127);
 			Lift.move(127);
@@ -64,14 +71,18 @@ void sort(void* sigPass)
 		else
 		if(Second_rtn.signature != 255)
 		{
+			#ifdef BLUE
 			vSensor.set_led(COLOR_RED);
+			#else
+			vSensor.set_led(COLOR_BLUE);
+			#endif
 			topLift.move(-127);
             bottomLift.move(127);
 			Lift.move(127);
 		}
 		#endif
 	
-		//so the program don't starve other tasks like updating the LCD
+		//make the thread sleep to prevent other threads from being starved of resources.
 		pros::Task::delay(10);
 	}
 }
